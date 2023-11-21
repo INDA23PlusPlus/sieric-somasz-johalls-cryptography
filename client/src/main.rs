1216e3725233f1b91c92f0f07b9ad5f8bda4eea2
Soma Szabo <szabosonka@gmail.com>
2023-11-21 16:57:15 +0100

    Work in progress send_data

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,27 @@
+use libhej::{Get, MessageType, Put};
+use std::io::prelude::*;
+use std::io::stdin;
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    let mut addr = String::new();
+    println!("Type the adress of the server: ");
+    stdin().read_line(&mut addr).unwrap();
+
+    let data: MessageType = MessageType::Put(Put {
+        id: String::from("123"),
+        data: String::from("hej grabs"),
+    });
+    send_data(addr, data);
+}
+
+fn send_data(addr: String, data: MessageType) -> std::io::Result<()> {
+    let mut stream = TcpStream::connect(addr)?;
+    match &data {
+        MessageType::Get(_) => panic!("Wrong MessageType"),
+        MessageType::Put(_) => {
+            serde_json::to_writer(&stream, &data);
+            Ok(())
+        }
+    }
 }
