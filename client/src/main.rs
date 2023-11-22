4bd74078488e394be8bc5dbbdc761ce510ccf9eb
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-11-22 20:54:14 +0100

    add verification of hashes of files from server

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,6 +3,7 @@ use libhej;
 use ring::digest;
 use std::io::prelude::*;
 use std::io::stdin;
+use std::str::from_utf8_unchecked;
 
 struct User {
     username: String,
@@ -13,6 +14,13 @@ struct ObfuscatedFileName {
     name: String,
 }
 
+fn get_hash(s: &str) -> String {
+    format!("{:?}", digest::digest(&digest::SHA512, s.as_bytes()))
+                .chars()
+                .skip(7)
+                .collect()
+}
+
 impl From<(User, String)> for ObfuscatedFileName {
     fn from(inp: (User, String)) -> Self {
         let (user, file_name) = inp;
@@ -20,7 +28,7 @@ impl From<(User, String)> for ObfuscatedFileName {
         data.push_str(&user.username);
         data.push_str(&user.password);
         ObfuscatedFileName {
-            name: format!("{:?}", digest::digest(&digest::SHA512, data.as_bytes())).chars().skip(7).collect(),
+            name: get_hash(data.as_str()),
         }
     }
 }
@@ -32,6 +40,21 @@ struct File {
 
 fn main() -> Result<()> {
     let mut buffer = String::new();
+
+    let mut files = std::collections::HashMap::<String, String>::new();
+    if let Ok(mut file) = std::fs::File::open("opened_files.cache") {
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+
+        for line in buf.lines() {
+            let line = line?;
+            let mut parts = line.split_ascii_whitespace();
+            let name = parts.next().unwrap();
+            let hash = parts.next().unwrap();
+            files.insert(name.to_owned(), hash.to_owned());
+        }
+    }
+
     loop {
         buffer.clear();
         stdin().read_line(&mut buffer)?;
@@ -58,14 +81,39 @@ fn main() -> Result<()> {
                     .create(true)
                     .write(true)
                     .open(parts[1])?;
-                file.write_all(b"From server")?;
+
+                let from_server = "From server".to_owned();
+
+                file.write_all(from_server.as_bytes())?;
+                if let Some(r) = files.get_mut(parts[1]) {
+                    if *r != get_hash(from_server.as_str()) {
+                        println!("FILE CONTENTS DO NOT MATCH");
+                        println!("Local hash: {}", *r);
+                        println!("Hash of file from server {}", get_hash(from_server.as_str()));
+                    }
+                } else {
+                    println!("WARNING, no local hash of file '{}'", parts[1]);
+                }
+
             }
             "put" => {
                 let mut file = std::fs::File::open(parts[1])?;
-                buffer.clear();
+                let mut buffer = String::new();
                 file.read_to_string(&mut buffer)?;
-
                 eprintln!("simulating sending to server: '{}'", buffer);
+                files.insert(parts[1].to_owned(), get_hash(buffer.as_str()));
+                let mut outp = String::new();
+                for (file, hash) in &files {
+                    outp.push_str(&file);
+                    outp.push(' ');
+                    outp.push_str(&hash);
+                    outp.push('\n');
+                }
+                let mut files_cache = std::fs::OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .open("opened_files.cache")?;
+                files_cache.write_all(outp.as_bytes())?;
             }
             _ => {
                 dbg_usage();
